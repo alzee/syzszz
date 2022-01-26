050765d98c05e374efda74fdcc2fd223ee3ceec9
Al Zee <z@alz.ee>
2022-01-26 11:58:57 +0800

    nth

--- web/themes/fxkyss/js/initSwiper.js
+++ web/themes/fxkyss/js/initSwiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper.slide1', {
   loop: true,
   autoplay: {
       delay: 3000,
@@ -14,5 +14,15 @@ const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.swiper.slide2', {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
   },
 });
